24f50e8493b26a1547e2783f72fba7e07e021d8b
vothibichngoc-dau <ngoc_2451220012@dau.edu.vn>
2026-03-24 09:14:45 +0700

    Add files via upload

---
+++ countMatches.py
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int index = 0;
+        if (ruleKey == "color") index = 1;
+        else if (ruleKey == "name") index = 2;
+        
+        int count = 0;
+        for (auto &item : items) {
+            if (item[index] == ruleValue) count++;
+        }
+        
+        return count;
+    }
+};
