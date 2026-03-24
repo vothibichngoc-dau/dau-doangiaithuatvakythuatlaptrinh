c02fcc5ea7117436331c6c0911dafae4418dd3e3
vothibichngoc-dau <ngoc_2451220012@dau.edu.vn>
2026-03-24 08:25:22 +0700

    Add files via upload

---
+++ numWaterBottles.py
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+        
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            total += newBottles;
+            
+            empty = newBottles + (empty % numExchange);
+        }
+        
+        return total;
+    }
+};
